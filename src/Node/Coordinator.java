6e0e695cb69c3b27d4c4e96cd01ad898e08a534b
Ashwin <ashwin.joshi12@live.com>
2017-12-01 19:45:15 -0600

    failure\timeout transitions added with recovery mechanisms

--- src/Node/Coordinator.java
+++ src/Node/Coordinator.java
@@ -30,6 +30,7 @@ import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -59,7 +60,7 @@ public class Coordinator{
             coordinatorSocket.setSoTimeout(60*60*1000);
             System.out.println("Coordinator started at "+coordinatorSocket.getLocalSocketAddress()+".");
         } catch (IOException ex) {
-            Logger.getLogger(Coordinator.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(Coordinator.class.getName()).log(Level.SEVERE, null, ex);
         }
         int p = pid;
         while(p<=con.N){
@@ -69,7 +70,7 @@ public class Coordinator{
                 pSockets.put(processSocket, p);
                 new processHandler(pid, processSocket, pSockets).start();
             } catch (IOException ex) {
-                Logger.getLogger(Coordinator.class.getName()).log(Level.SEVERE, null, ex);
+                //Logger.getLogger(Coordinator.class.getName()).log(Level.SEVERE, null, ex);
             }
             
         }
@@ -78,9 +79,15 @@ public class Coordinator{
 
 
 class processHandler extends Thread{
+    
     static int agreedCount = 0;
     static int readyCount = 0;
-    static long t1, t2;
+    static long t1, t2, t3, t4;
+    static long t5, t6;
+    static int fcount = 0;
+    static String stateInfo; 
+    String recoveryFileName = "coordinator.log";
+    
     Map<Socket, Integer> pSockets;
     Constants con = new Constants();
     static Map<ObjectOutputStream, Integer> pList = new HashMap<>();
@@ -116,14 +123,24 @@ class processHandler extends Thread{
                 fileWriter.close();
             }
         } catch (IOException ex) {
-            Logger.getLogger(processHandler.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(processHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
         
     }
-    
+    /**
+     * Pretty print messages within the 
+     * program.
+     */
     public void print(Object o){
         System.out.println("[coordinator@"+con.coordinator+"]$:"+o.toString());
     }
+    /**
+     * A generic send method to send
+     * any kind of object to the cohorts.
+     * The object can any data structure so
+     * it is used to send all sorts of
+     * messages.
+     */
     public void sendTo(Integer i, Object o){
         for(ObjectOutputStream out : pList.keySet()){
             if(pList.get(out).equals(i)){
@@ -131,22 +148,129 @@ class processHandler extends Thread{
                     out.writeObject(o);
                     out.flush();
                 } catch (IOException ex) {
-                    Logger.getLogger(processHandler.class.getName()).log(Level.SEVERE, null, ex);
+                    //Logger.getLogger(processHandler.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 print("Sent: "+o);
             }
         }
     }
-    
+    /**
+     * A broadcast method to send a 
+     * message to all the cohorts
+     * at once.
+     */
     private void sendAll(Object o){
         for(Integer i : pList.values()){
             sendTo(i, o);
         }
     }
+    /**
+     * A method to initiate the final commit.
+     * In our case, the coordinator commits a 
+     * value, so ideally everyone should commit
+     * the same value.
+    */
+    public void commitValue(){
+        print("Please enter a value to commit.");
+        Object val = sc.nextInt();
+        Commit commitMsg = new Commit(pid);
+        commitMsg.setval(val);
+        sendAll(commitMsg);
+        stateInfo = "c";
+        if(con.coordinatorFailState.equalsIgnoreCase(stateInfo)){
+            fail(stateInfo);
+        }
+        sc.close();
+        save_state(transaction_id, val);
+        System.exit(0);
+    }
+    
+    /**
+     * Failure transition steps.
+     */
+    public void fail(String state){
+        print("Failure at="+state);
+        failureTransition();
+        System.exit(1);
+    }
+    /**
+     * Recovery mechanism.
+     */
+    public void recoverFromLog(){
+        Abort abortMsg;
+        try {
+            File f = new File(recoveryFileName);
+            if(f.exists() && !f.isDirectory()){
+                BufferedReader bf = new BufferedReader(new FileReader(f));
+                String line;
+                line = bf.readLine();
+                String failState;
+                failState = line.split("=")[1];
+                bf.close();
+                f.delete();
+                switch(failState){
+                    case "q":
+                        stateInfo = "q";
+                        print("Recovery from="+failState);
+                        abortMsg = new Abort(pid);
+                        sendAll(abortMsg);
+                        stateInfo = "a";
+                        System.exit(0);
+                        break;
+                    case "w":
+                        stateInfo = "w";
+                        print("Recovery from="+failState);
+                        abortMsg = new Abort(pid);
+                        sendAll(abortMsg);
+                        stateInfo = "a";
+                        System.exit(0);
+                        break;
+                    case "p":
+                        stateInfo = "p";
+                        print("Recovery from="+failState);
+                        commitValue();
+                        break;
+                    default : 
+                        print("No recovery state found.");
+                        break;
+                }
+            }
+            
+            
+        } catch (FileNotFoundException ex) {
+            //Logger.getLogger(processHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            //Logger.getLogger(processHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    /**
+     * Initiates the runtime shutdown hook thread
+     * to capture the interrupt signal SIGINT 
+     * which is the user interrupt to cause this 
+     * program to fail. With this, its possible
+     * to store the failure state and use to it
+     * to do the required failure transition.
+     */
+    public void failureTransition(){
+        PrintWriter pw = null;
+        try {
+            File f = new File(recoveryFileName);
+            pw = new PrintWriter(new FileWriter(f));
+            pw.println("fail_state="+stateInfo);
+            pw.flush();
+        } catch (IOException ex) {
+            //Logger.getLogger(processHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            pw.close();
+        }
+        
+    }
+    
     @Override
     public void run(){
         
         try {
+            t5 = System.currentTimeMillis();
             print("Connected to "+processSocket.getRemoteSocketAddress());
             ObjectOutputStream objout = new ObjectOutputStream(processSocket.getOutputStream());
             ObjectInputStream objin = new ObjectInputStream(processSocket.getInputStream());
@@ -156,6 +280,7 @@ class processHandler extends Thread{
                 Object recvMsg;
                     recvMsg = objin.readObject();
                     if(recvMsg instanceof Register){
+                        
                         print("Received: "+recvMsg);
                         if(pSockets.containsKey(processSocket)){
                             pList.put(objout, pSockets.get(processSocket));
@@ -168,10 +293,24 @@ class processHandler extends Thread{
                                 sendMsg.setpid(i);
                                 sendTo(i, sendMsg);
                             }
+                            recoverFromLog();
                             
                             Request commitRequest = new Request(pid);
-                            commitRequest.settext("Commit Request");
+                            t6 = System.currentTimeMillis();
                             sendAll(commitRequest);
+                            print("diff="+Math.abs(t6-t5));
+                            stateInfo = "q";
+                            if(Math.abs(t6-t5) >= con.tq1){
+                                print("Coordinator timed out.");
+                                Abort abortMsg = new Abort(pid);
+                                sendAll(abortMsg);
+                                print("Aborting...");
+                                Thread.sleep(6000);
+                                System.exit(0);
+                            }
+                            if(con.coordinatorFailState.equalsIgnoreCase(stateInfo)){
+                                fail(stateInfo);
+                            }
                             t1 = System.currentTimeMillis();
                         }
                     }
@@ -181,18 +320,26 @@ class processHandler extends Thread{
                         if(((Ack)recvMsg).gettext().equalsIgnoreCase("Agree")){
                             agreedCount++;
                             if(agreedCount == (con.N-1)){
+                                stateInfo = "w";
+                                if(con.coordinatorFailState.equalsIgnoreCase(stateInfo)){
+                                    fail(stateInfo);
+                                }
                                 t2 = System.currentTimeMillis();
-                                if(Math.abs(t1-t2) >= 10000){
+                                if(Math.abs(t1-t2) >= con.tw1){
                                     print("Coordinator timed out.");
                                     Coordinator.coordinatorSocket.close();
                                     Abort abortMsg = new Abort(pid);
-                                    abortMsg.settext("Abort");
                                     sendAll(abortMsg);
+                                    print("Aborting now...");
                                     System.exit(0);
                                 }
                                 Prepare prepareCommit = new Prepare(pid);
-                                prepareCommit.settext("Prepare to commit");
                                 sendAll(prepareCommit);
+                                stateInfo = "p";
+                                if(con.coordinatorFailState.equalsIgnoreCase(stateInfo)){
+                                    fail(stateInfo);
+                                }
+                                t3 = System.currentTimeMillis();
                             }
                         }
                         
@@ -200,31 +347,35 @@ class processHandler extends Thread{
                             readyCount++;
                             
                             if(readyCount == (con.N-1)){
-                                print("Please enter a value to commit.");
-                                Object val = sc.nextInt();
-                                Commit commitMsg = new Commit(pid);
-                                commitMsg.settext("Commit");
-                                commitMsg.setval(val);
-                                sendAll(commitMsg);
-                                sc.close();
-                                save_state(transaction_id, val);
-                                System.exit(0);
+                                t4 = System.currentTimeMillis();
+                                if(Math.abs(t4-t3)>=con.tp1){
+                                    print("Coordinator timed out.");
+                                    Coordinator.coordinatorSocket.close();
+                                    Abort abortMsg = new Abort(pid);
+                                    sendAll(abortMsg);
+                                    print("Aborting now...");
+                                    System.exit(0);
+                                }
+                                commitValue();
                             }
                         }
                     }
                     
                     if(recvMsg instanceof Abort){
+                        stateInfo = "w";
                         print("Received: "+recvMsg);
                         Abort abortMsg = new Abort(pid);
-                        abortMsg.settext("Abort");
                         sendAll(abortMsg);
+                        stateInfo = "a";
                         System.exit(0);
                     }
             }
         } catch (IOException ex) {
-            Logger.getLogger(processHandler.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(processHandler.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(processHandler.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(processHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException ex) {
+            //Logger.getLogger(processHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
--- src/Node/Process.java
+++ src/Node/Process.java
@@ -11,9 +11,12 @@ import Message.Commit;
 import Message.Prepare;
 import Message.Register;
 import Message.Request;
+import static Node.processHandler.fcount;
+import Utils.Constants;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -39,8 +42,13 @@ public class Process{
     PrintWriter fileWriter = null;
     static int transaction_id = 0;
     Scanner sc = new Scanner(System.in);
+    Constants con = new Constants();
     String file_name;
     File file;
+    String recoveryFileName = "cohort.log";
+    
+    static long t1,t2,t3,t4,t5,t6;
+    static String stateInfo;
     
     public Process(String hostName, int port){
         this.hostName = hostName;
@@ -59,9 +67,9 @@ public class Process{
     }
     
     public void reconnect(){
-        print(processSocket);
-        print(processSocket.isClosed());
+        print("Attempting to reconnect to coordinator...");
     }
+    
     private void save_state(int transaction_id, Object val){
         try {
             file = new File(file_name);
@@ -75,31 +83,143 @@ public class Process{
             }
             
         } catch (IOException ex) {
-            Logger.getLogger(Process.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(Process.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    /**
+     * Pretty print all kinds of messages.
+     */
     private void print(Object o){
         System.out.println("[process@"+processSocket.getLocalSocketAddress()+"]$:"+o.toString());
     }
+    
+    /**
+     * Generic send method to send any kind of
+     * message or object. 
+     * Allows to send other values or 
+     * data structures.
+     */
     private void send(ObjectOutputStream out, Object o){
         try {
             out.writeObject(o);
             out.flush();
             print("Sent: "+o);
         } catch (IOException ex) {
-            Logger.getLogger(Process.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(Process.class.getName()).log(Level.SEVERE, null, ex);
         }
         
     }
-
+    /**
+     * Method to initiate failure transition
+     * depending the state at which it has
+     * failed.
+     */
+    public void fail(String state){
+        print("Failure at="+state);
+        failureTransition();
+        System.exit(1);
+    }
+    /**
+     * Recovery mechanism to recover from 
+     * a failed state and then perform
+     * failure transition operation
+     * as per the 3-Phase commit protocol.
+     */
+    public void recoverFromLog(ObjectOutputStream objOut){
+        Abort abortMsg;
+        try {
+            File f = new File(recoveryFileName);
+            if(f.exists() && !f.isDirectory()){
+                BufferedReader bf = new BufferedReader(new FileReader(f));
+                String line;
+                line = bf.readLine();
+                String failState;
+                failState = line.split("=")[1];
+                bf.close();
+                f.delete();
+                switch(failState){
+                    case "q":
+                        stateInfo = "q";
+                        print("Recovery from="+failState);
+                        print("Aborting...");
+                        abortMsg = new Abort(pid);
+                        send(objOut, abortMsg);
+                        stateInfo = "a";
+                        System.exit(0);
+                        break;
+                    case "w":
+                        stateInfo = "w";
+                        print("Recovery from="+failState);
+                        print("Aborting...");
+                        abortMsg = new Abort(pid);
+                        send(objOut, abortMsg);
+                        stateInfo = "a";
+                        System.exit(0);
+                        break;
+                    case "p":
+                        stateInfo = "p";
+                        print("Recovery from="+failState);
+                        //commitValue();
+                        break;
+                    default : 
+                        print("No recovery state found.");
+                        break;
+                }
+                
+            }
+            
+            
+        } catch (FileNotFoundException ex) {
+            //Logger.getLogger(processHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            //Logger.getLogger(processHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    /**
+     * Actions performed during failure 
+     * transition for cohort.
+     */
+    public void failureTransition(){
+        PrintWriter pw = null;
+        try {
+            File f = new File(recoveryFileName);
+            pw = new PrintWriter(new FileWriter(f));
+            pw.println("fail_state="+stateInfo);
+            pw.flush();
+        } catch (IOException ex) {
+            //Logger.getLogger(processHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            pw.close();
+        }
+    }
+    /**
+     * Method invoked to start the process 
+     * program.
+     */
     public void start(){
         try {
             print("Connection established with "+hostName+"@"+port+".");
+            t1 = System.currentTimeMillis();
             ObjectOutputStream objout = new ObjectOutputStream(processSocket.getOutputStream());
             ObjectInputStream objin = new ObjectInputStream(processSocket.getInputStream());
             
             Register registerMsg = new Register("Register");
             send(objout, registerMsg);
+            t2 = System.currentTimeMillis();
+            print("diff="+Math.abs(t2-t1));
+            /**
+             * Timeout transition during state "q"
+             * for cohort "i".
+             */
+            if(Math.abs(t2-t1) >= con.tqi){
+                print("Cohort timed out.");
+                Abort abortMsg = new Abort(pid);
+                send(objout, abortMsg);
+                print("Aborting...");
+                System.exit(0);
+            }
+            recoverFromLog(objout);
             while(true){
                 if(processSocket.isConnected()){
                     Object recvMsg = objin.readObject();
@@ -108,45 +228,105 @@ public class Process{
                         print("Received: "+recvMsg);
                         pid = ((Register) recvMsg).getpid();
                         this.file_name = "state_"+pid+".out";
+                        stateInfo = "q";
+                        /**
+                         * Failure transition during state "q".
+                         */
+                        if(con.cohortFailState.equalsIgnoreCase(stateInfo) && (con.failingCohort == pid) ){
+                            fail(stateInfo);
+                        }
                     }
                     
                     if(recvMsg instanceof Request){
                         print("Received: "+recvMsg);
+                        t3 = System.currentTimeMillis();
                         print("Do you wish to commit ? (y/n)");
                         String text = sc.nextLine();
                         Ack sendAck = new Ack(pid);
                         if(text.equalsIgnoreCase("y")){
                             sendAck.settext("Agree");
                             send(objout, sendAck);
+                            t4 = System.currentTimeMillis();
+                            print("diff="+Math.abs(t4-t3));
+                            if(Math.abs(t4-t3) >= con.twi){
+                                print("Cohort timed out.");
+                                Abort abortMsg = new Abort(pid);
+                                send(objout, abortMsg);
+                                print("Aborting...");
+                                System.exit(0);
+                            }
+                            stateInfo = "w";
+                            /**
+                            * Failure transition during state "w".
+                            */
+                            if(con.cohortFailState.equalsIgnoreCase(stateInfo) && (con.failingCohort == pid) ){
+                                fail(stateInfo);
+                            }
                         }else{
                             Abort abortMsg = new Abort(pid);
-                            abortMsg.settext("Abort");
                             send(objout, abortMsg);
+                            t4 = System.currentTimeMillis();
+                            print("diff="+Math.abs(t4-t3));
+                            stateInfo = "a";
+                            if(Math.abs(t4-t3) >= con.twi){
+                                print("Cohort timed out.");
+                                //abortMsg = new Abort(pid);
+                                //send(objout, abortMsg);
+                                print("Aborting...");
+                                System.exit(0);
+                            }
+                            /**
+                            * Failure transition during state "a".
+                            */
+                            if(con.cohortFailState.equalsIgnoreCase(stateInfo) && (con.failingCohort == pid) ){
+                                fail(stateInfo);
+                            }
                             System.exit(0);
                         }
                     }
-                    print(processSocket.isOutputShutdown());
-                    if(processSocket.isInputShutdown()){
-                        print("Timeout");
-                        System.exit(0);
-                    }
+                    
                     if(recvMsg instanceof Prepare){
                         print("Received: "+recvMsg);
                         Ack sendAck = new Ack(pid);
                         sendAck.settext("Ready to commit");
                         send(objout, sendAck);
+                        t5 = System.currentTimeMillis();
+                        stateInfo = "p";
+                        if(con.cohortFailState.equalsIgnoreCase(stateInfo) && (con.failingCohort == pid) ){
+                            fail(stateInfo);
+                        }
                     }
                     
                     if(recvMsg instanceof Commit){
+                        t6 = System.currentTimeMillis();
                         print("Received: "+recvMsg);
+                        print("diff="+Math.abs(t6-t5));
+                        if(Math.abs(t6-t5) >= con.tpi){
+                            print("Cohort timed out.");
+                            print("received value="+((Commit) recvMsg).getval());
+                            save_state(transaction_id, ((Commit) recvMsg).getval());
+                            stateInfo = "c";
+                            if(con.cohortFailState.equalsIgnoreCase(stateInfo) && (con.failingCohort == pid) ){
+                                fail(stateInfo);
+                            }
+                            System.exit(0);
+                        }
                         print("Received value="+((Commit) recvMsg).getval());
                         save_state(transaction_id, ((Commit) recvMsg).getval());
+                        stateInfo = "c";
+                        if(con.cohortFailState.equalsIgnoreCase(stateInfo) && (con.failingCohort == pid) ){
+                            fail(stateInfo);
+                        }
                         System.exit(0);
                     }
                     
                     if(recvMsg instanceof Abort){
                         print("Received: "+recvMsg);
                         print("Aborting...");
+                        stateInfo = "a";
+                        if(con.cohortFailState.equalsIgnoreCase(stateInfo) && (con.failingCohort == pid) ){
+                            fail(stateInfo);
+                        }
                         System.exit(0);
                     }
                 }else{
@@ -156,7 +336,7 @@ public class Process{
         } catch (IOException ex) {
             //Logger.getLogger(Process.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Process.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(Process.class.getName()).log(Level.SEVERE, null, ex);
         } 
         
     }
