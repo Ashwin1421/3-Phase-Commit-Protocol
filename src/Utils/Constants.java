cb77bcc4a6fdef1ef9e92d5ffbaa3c78a9cbc5dc
Ashwin <ashwin.joshi12@live.com>
2017-12-01 19:45:39 -0600

    updated config values

--- src/Utils/Constants.java
+++ src/Utils/Constants.java
@@ -20,6 +20,11 @@ public class Constants {
     InputStream configFileInputStream = null;
     public String coordinator = null;
     public Integer N;
+    public long tq1, tw1, tp1;
+    public long tqi, twi, tpi;
+    public String coordinatorFailState;
+    public String cohortFailState;
+    public int failingCohort;
 
     public Constants(){
         configFileInputStream = getClass().getClassLoader().getResourceAsStream("config.properties");
@@ -35,6 +40,15 @@ public class Constants {
         
         this.coordinator = prop.getProperty("coordinator");
         this.N = Integer.parseInt(prop.getProperty("n"));
+        this.tq1 = Long.parseLong(prop.getProperty("tq1"));
+        this.tw1 = Long.parseLong(prop.getProperty("tw1"));
+        this.tp1 = Long.parseLong(prop.getProperty("tp1"));
+        this.tqi = Long.parseLong(prop.getProperty("tqi"));
+        this.twi = Long.parseLong(prop.getProperty("twi"));
+        this.tpi = Long.parseLong(prop.getProperty("tpi"));
+        this.coordinatorFailState = prop.getProperty("f1");
+        this.cohortFailState = prop.getProperty("fi");
+        this.failingCohort = Integer.parseInt(prop.getProperty("i"));
     }
     
 }
